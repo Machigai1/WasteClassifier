c0b1865868ebf0dda736cf0eca1a72dc8e3511e5
Jiyan0102 <carloperos23@gmail.com>
2025-03-26 13:47:57 +0800

    Update mlearn.py

--- mlearn.py
+++ mlearn.py
@@ -56,7 +56,7 @@ def predict_waste(image, filename):
 st.set_page_config(page_title="Waste Classifier", page_icon="♻️", layout="wide")
 
 # Background GIF using CSS
-background_gif = "https://www.icegif.com/wp-content/uploads/2023/07/icegif-86.gif"  # Replace with your GIF URL or path
+background_gif = "https://images.steamusercontent.com/ugc/1654474928695116067/526D905CC08F313F3BB854AA256D07CF37E15B18/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"  # Replace with your GIF URL or path
 
 st.markdown(
     f"""
